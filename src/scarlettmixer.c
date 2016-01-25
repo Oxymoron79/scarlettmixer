1b5f98bb7717cc88c2f6fc18f401b896613ba467
Martin Roesch <martin.roesch79@gmail.com>
2016-01-25 23:28:58 +0100

    Added license header.

--- src/scarlettmixer.c
+++ src/scarlettmixer.c
@@ -1,11 +1,19 @@
 /*
- ============================================================================
- Name        : scarlettmixer.c
- Author      : Martin Rösch
- Version     :
- Copyright   : Copyright (C) 2015
- Description : Hello World in C, Ansi-style
- ============================================================================
+ * scarlettmixer - GTK+ mixer for scarlett audio interfaces.
+ * Copyright (c) 2016 Martin Roesch <martin.roesch79@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include "sm-app.h"
